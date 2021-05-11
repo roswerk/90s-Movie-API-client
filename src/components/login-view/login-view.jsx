3aa63602d23eb2e3991434d68d2b06f30cd40705
Max <73542409+roswerk@users.noreply.github.com>
2021-05-11 18:23:42 +0200

    Updated axios to heroku api route

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -16,16 +16,22 @@ export function LoginView(props) {
   const handleSubmit = (e) => {
     e.preventDefault();
     /* Send a request to the server for authentication */
-    axios.post ("https://api90smovies.herokuapp.com/login", {
+    axios.post ("https://api90smovies.herokuapp.com/login", {  
+    /*  axios.post ("http://localhost:8080/login", { */
       userName: username,
       password: password
     })
     .then(response => {
       const data = response.data;
+      console.log(data);
       props.onLoggedIn(data);
     })
     .catch(e => {
       console.log("User with such characteristics not found")
+      
+    
+      console.log('User JSON: ' +username);
+      console.log('Pass JSON: ' +password);
     })
   };
 
