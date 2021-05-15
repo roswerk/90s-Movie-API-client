e8d00d6f3417a7c92282e965f9ae0dc46ee11a40
Max <73542409+roswerk@users.noreply.github.com>
2021-05-15 17:49:11 +0200

    Updated movie card display of genre and director

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -5,22 +5,29 @@ import Card from "react-bootstrap/Card";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import "./movie-card.scss";
+import { Link } from "react-router-dom"; 
 
 
 export class MovieCard extends React.Component{
   render(){
     const {movie, onMovieClick} = this.props;
 
+
+
     return (
    
-    <Card bg="light" border="danger" className="movieCards">
-      <Card.Img variant="top" src={movie.imageURL} className="poster"/>
+    <Card bg="light" border="danger" className="movieCards mb-3">
+      <Card.Img variant="top" src={movie.imageURL}/>
       <Card.Body >
         <Card.Title>{movie.title}</Card.Title>
         <Card.Text className="movie-text">{movie.description}</Card.Text>
-        <Button onClick={() => onMovieClick(movie)} variant="danger" >Open</Button>
+        <Card.Text className="movie-text">{movie.genre.name}</Card.Text>
+        <Link to={`/movies/${movie._id}`}>
+        <Button variant="danger link">Open</Button>
+        </Link>
       </Card.Body>
     </Card>
+    
     );
   }
 }
@@ -33,14 +40,13 @@ MovieCard.propTypes = {
     imageURL: PropTypes.string.isRequired,
     genre: PropTypes.shape({
       name: PropTypes.string.isRequired,
-      description: PropTypes.string.isRequired
+      description: PropTypes.string
     }),
     director: PropTypes.shape({
-      name: PropTypes.string.isRequired,
-      bio: PropTypes.string.isRequired,
-      placeOfBirth: PropTypes.string.isRequired,
-      birthDate: PropTypes.string.isRequired
+      name: PropTypes.string,
+      bio: PropTypes.string,
+      placeOfBirth: PropTypes.string,
+      birthDate: PropTypes.string
     })
-  }).isRequired,
-  onMovieClick: PropTypes.func.isRequired
+  }).isRequired
 };
