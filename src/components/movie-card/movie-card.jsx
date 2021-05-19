47e74dc66753b16a31d4d4f7375a448e9c63c1d5
Max <73542409+roswerk@users.noreply.github.com>
2021-05-19 22:50:36 +0200

    Updated favMovie feature

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -2,8 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+import axios from "axios";
 import "./movie-card.scss";
 import { Link } from "react-router-dom"; 
 
@@ -12,10 +11,26 @@ export class MovieCard extends React.Component{
   render(){
     const {movie, onMovieClick} = this.props;
 
-
+    function addFav (e) {
+      e.preventDefault();
+  
+      axios.post(`https://api90smovies.herokuapp.com/users/${localStorage.getItem('user')}/favMovies/${movie._id}`,
+      {
+        username: localStorage.getItem("user")
+      },
+      {
+        headers: { Authorization: `Bearer ${localStorage.getItem("token")}` }
+      }
+      ).then(response => {
+        localStorage.setItem("favoriteMovies", JSON.stringify(response.data.favoriteMovies));
+        alert("Movie has been added to your Favorite list!");
+        }). catch (e => {
+          console.log("Movie can't be added to your Favorites, because" + e );
+          alert("Fav movie already included, can't be readded");
+        });
+    }
 
     return (
-   
     <Card bg="light" border="danger" className="movieCards mb-3">
       <Card.Img variant="top" src={movie.imageURL}/>
       <Card.Body >
@@ -25,6 +40,7 @@ export class MovieCard extends React.Component{
         <Link to={`/movies/${movie._id}`}>
         <Button variant="danger link">Open</Button>
         </Link>
+        <Button onClick={e => addFav(e)} variant="danger" className="ml-3"><span>&#129293;</span></Button>
       </Card.Body>
     </Card>
     
