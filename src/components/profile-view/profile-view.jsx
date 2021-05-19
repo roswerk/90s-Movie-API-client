f1a5b6ba732a146d28913d58b3f593a5d0cfc94c
Max <73542409+roswerk@users.noreply.github.com>
2021-05-19 20:43:35 +0200

    Refactoring profile-view component

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -5,20 +5,10 @@ import Button from "react-bootstrap/Button"
 import ListGroup from "react-bootstrap/ListGroup";
 import ListGroupItem from "react-bootstrap/ListGroupItem";
 import axios from 'axios';
-import { Link } from 'react-router-dom';
-
 
 // Modal buttons
 let editProfile =  function(){
-  let edit = document.getElementsByClassName("editProfile")[0].toggleAttribute("hidden")
-  let view = document.getElementsByClassName("viewProfile")[0].toggleAttribute("hidden")
   let defaultKey = document.getElementsByTagName("Nav")[0].toggleAttribute("edit")
-  
-}
-let profile = function (){
-  let profile = document.getElementsByClassName("viewProfile")[0].toggleAttribute("hidden")
-  let edit = document.getElementsByClassName("editProfile")[0].toggleAttribute("hidden")
-  let defaultKey = document.getElementsByTagName("Nav")[0].toggleAttribute("first")
 }
 
 export class ProfileView extends React.Component{
@@ -33,6 +23,8 @@ export class ProfileView extends React.Component{
     };
   }
 
+  
+
   componentDidMount() {
     let accessToken = localStorage.getItem('token');
     if (accessToken !== null) {
@@ -44,7 +36,7 @@ export class ProfileView extends React.Component{
     this.setState({
       userName: localStorage.getItem('user'),
       email: localStorage.getItem('email'),
-      birthDate: localStorage.getItem('birthDate').slice(1,11),
+      birthDate: localStorage.getItem('birthDate').slice(0,11),
       favoriteMovies: JSON.parse(localStorage.getItem('favoriteMovies'))
     });
   }
@@ -69,10 +61,9 @@ removeFavorite(favorite) {
   });
 }
 
-
-
-  render(){
+  render(){    
     const { userName, email, birthDate, favoriteMovies} = this.state;
+    
     
     const favoriteMovieList = this.props.movies.filter((movie => {
       return (
@@ -80,32 +71,43 @@ removeFavorite(favorite) {
       );
     }));
 
-      //Delete Profile
-  const handleDelete = e => {
+  //Update profile
+  const handleUpdate = e => {
     e.preventDefault();
-    axios
-      .delete(`https://api90smovies.herokuapp.com/users/delete/${userName}`, {
-        headers: { Authorization: `Bearer ${localStorage.getItem("token")}` }
-      })
-      .then(response => {
-        alert("Your account has been deleted");
-        localStorage.clear();
-        window.open("/", "_self");
+    const userInfo = {
+      userName: username,
+      password: password,
+      email: email,
+      birthDate: birthday
+    };
+
+    axios.put(`https://api90smovies.herokuapp.com/user/${userName}`, userInfo, {
+      headers: { Authorization: `Bearer ${localStorage.getItem('token')}` }
+    })
+    .then(response => {
+      updateUser(userInfo);
+      alert("Profile has been Updated");
       })
       .catch(e => {
-        console.log("Error deleting your account");
+        const errors = e.response.data.errors || [];
+        let errorMessage = "";
+        errors.forEach(err => {
+          errorMessage += err.msg;
+        });
+        alert(`There was an error ${errorMessage}`);
+        console.log(`Error updating the user info.`);
       });
   };
 
 return(
 <Card>
   <Card.Header>
-    <Nav variant="tabs" defaultActiveKey="#first">
+    <Nav variant="tabs" defaultActiveKey="/profile">
       <Nav.Item>
-        <Nav.Link href="#first" onClick={profile}>Profile</Nav.Link>
+        <Nav.Link href="/profile">Profile</Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link href="#edit" onClick={editProfile}>Edit profile</Nav.Link>
+        <Nav.Link href="/update" onClick={editProfile}>Edit profile</Nav.Link>
       </Nav.Item>
     </Nav>
   </Card.Header>
@@ -119,43 +121,19 @@ return(
     <ListGroupItem> Username: <span className='text-color'>{userName}</span></ListGroupItem>
     <ListGroupItem> Email: <span className='text-color'>{email}</span></ListGroupItem>
     <ListGroupItem> Birth date: <span className='text-color'>{birthDate}</span></ListGroupItem>
-     <ListGroupItem className="mt-5"> My Favorite Movies:   <div>
-                    <ListGroup variant="flush">
-                      {favoriteMovieList.map(favoriteMovies => (
-                        <ListGroup.Item key={favoriteMovies._id} >
-                          <Button onClick={() => this.removeFavorite(favoriteMovies)} variant="light"><span className="light">X</span></Button> 
-                          <span className='text-color'>
-                          {
-                            favoriteMovies.title
-                          }
-                          </span>
-                                                    
-                        </ListGroup.Item>                        
-                      ))}
-                    </ListGroup> 
-                </div></ListGroupItem>  
+     <ListGroupItem className="mt-5"> My Favorite Movies:   
+     <div>
+            <ListGroup variant="flush">
+              {favoriteMovieList.map(favoriteMovies => (
+                  <ListGroup.Item key={favoriteMovies._id} >
+                  <Button onClick={() => this.removeFavorite(favoriteMovies)} size="sm" variant="outline-danger mr-3"><span className="light">Delete</span></Button> 
+                    <span className='text-color'>{favoriteMovies.title}</span>                   
+                  </ListGroup.Item>))}
+            </ListGroup> 
+      </div></ListGroupItem>  
   </ListGroup>
-    <Button variant="primary mt-5">Go somewhere</Button>
     </Card.Body>
-
-
-    <Card.Body hidden={true} className="editProfile">
-    <Card.Title>Edit Account</Card.Title>
-    <Card.Text>
-      Here you can update your details here or delete your account. But trust us, you wont want to &#128524;
-    </Card.Text>
-    
-    <ListGroup className="list-group-flush mt-4">
-    <ListGroupItem>Cras justo odio</ListGroupItem>
-    <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-    <ListGroupItem>Vestibulum at eros</ListGroupItem>
-  </ListGroup>
-    <Button variant="outline-danger" className="mt-5" type='submit' onClick={handleDelete}>Delete Account</Button>
-    </Card.Body>
-
 </Card>)
-
-    
   }}
 
   export default ProfileView;
