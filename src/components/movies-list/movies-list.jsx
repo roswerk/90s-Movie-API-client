442fe3a7f3578de2e3ef9848d19413cc018403ef
Max <73542409+roswerk@users.noreply.github.com>
2021-05-25 18:14:25 +0200

    Created movies-list to replace main-view display of movies

---
+++ src/components/movies-list/movies-list.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row"
+import {connect} from "react-redux";
+
+import VisibilityFilterInput from "../visibility-filter-input/visibility-filter-input";
+import {MovieCard} from "../movie-card/movie-card";
+
+const mapStateToProps = state => {
+  const {visibilityFilter} = state;
+  return {visibilityFilter};
+};
+
+function MoviesList(props){
+  const {movies, visibilityFilter} = props;
+  let filteredMovies = movies;
+
+if (visibilityFilter !== " "){
+  filteredMovies = movies.filter( m => m.title.toLowerCase().includes(visibilityFilter.toLowerCase()));
+}
+
+if(!movies) return <div className="main-view" />;
+
+return <>
+{/* <Col md={12} style={{margin: "1em"}}>
+  <VisibilityFilterInput visibilityFilter={visibilityFilter} />
+  </Col> */}
+    <Row>
+    {filteredMovies.map(m => (
+  <Col md={4} key={m._id}>
+    <MovieCard movie={m}/>
+  </Col>))}
+  </Row>
+</>
+};
+
+export default connect(mapStateToProps)(MoviesList);
+
