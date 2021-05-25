da5163d948a862c3a8e0b7c25af0647a1f3fa887
Max <73542409+roswerk@users.noreply.github.com>
2021-05-25 20:55:59 +0200

    Updated loggedIn reducers

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,5 +1,5 @@
 import {combineReducers} from "redux";
-import {SET_FILTER, SET_MOVIES} from "../actions/actions";
+import {SET_FILTER, SET_MOVIES, SET_USER} from "../actions/actions";
 
 function visibilityFilter(state = "", action){
   switch(action.type){
@@ -19,11 +19,19 @@ function movies(state = [], action){
   }
 }
 
-function moviesApp(state = {}, action){
-  return {
-    visibilityFilter: visibilityFilter(state.visibilityFilter, action),
-    movies: movies(state.movies, action)
+function loggedInUser(state = [], action) {
+  switch (action.type) {
+    case SET_USER:
+      return action.value;
+      default:
+        return state;
   }
 }
+
+const moviesApp = combineReducers({
+  visibilityFilter,
+  movies,
+  loggedInUser
+});
 
 export default moviesApp;
