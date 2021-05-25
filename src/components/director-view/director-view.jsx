34c5689def6f2f475097f86084ebc8d88235aec3
Max <73542409+roswerk@users.noreply.github.com>
2021-05-25 20:57:33 +0200

    Refactored director-view

--- src/components/director-view/director-view.jsx
+++ src/components/director-view/director-view.jsx
@@ -14,22 +14,27 @@ export class DirectorView extends React.Component{
     return(
 
     <div className="director-view">
+      <Row>
+        <Col>
       <div className="director-poster">
         <img src={director.directorImg} alt="Director Image" className="poster"/>
       </div>
+      </Col>
+      <Col>
       <div className="Director-name">
         <span className="label">Name: </span>
         <span className="value">{director.name}</span>
       </div>
-      <div className="Director-bio">
+      <div className="Director-bio mt-5">
         <span className="label">Bio: </span>
         <span className="value">{director.bio}</span>
       </div>
-      <div className="Director-DOB">
+      <div className="Director-DOB mt-5">
         <span className="label">Date of Birth: </span>
         <span className="value">{JSON.stringify(director.birthDate).slice(1,11)}</span>
       </div>
-         
+      </Col>
+      </Row>
  <Row className="mt-3">
    <Col>
       <Link to={"/"}>
